c0e2153cebb5d7644da45f3f1a12787a408fbb9e
Raji Ayyub <156102242+raji-ayyub@users.noreply.github.com>
2025-01-31 09:26:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 def read_root():
     current_datetime = datetime.now(pytz.UTC).strftime("%Y-%m-%dT%H:%M:%SZ")  # Correct format
     return {
-        "email": "your-email@example.com",
+        "email": "ayyubraji87@gmail.com",
         "current_datetime": current_datetime,
-        "github_url": "https://github.com/yourusername/your-repo"
+        "github_url": "https://github.com/raji-ayyub/BEtask"
     }
