a85ae62c6d1f4a994245195c75db061a805547ee
Raji Ayyub <156102242+raji-ayyub@users.noreply.github.com>
2025-01-31 09:10:43 +0100

    Fix: date-time format

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI
 from datetime import datetime
 import pytz
@@ -5,9 +6,10 @@ import pytz
 app = FastAPI()
 
 @app.get("/")
-def get_info():
+def read_root():
+    current_datetime = datetime.now(pytz.UTC).strftime("%Y-%m-%dT%H:%M:%SZ")  # Correct format
     return {
-        "email": "ayyubraji87@gmail.com",
-        "current_datetime": datetime.now(pytz.UTC).isoformat(),
-        "github_url": "https://github.com/raji-ayyub/BEtask"
+        "email": "your-email@example.com",
+        "current_datetime": current_datetime,
+        "github_url": "https://github.com/yourusername/your-repo"
     }
